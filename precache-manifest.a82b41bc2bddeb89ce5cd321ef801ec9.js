b1cb7cd8b76f1f33903dcf2f1f1c3989f0c2ef48
MateuszPuchalski <puchalskimateusz1@gmail.com>
2020-03-21 02:57:50 +0100

    Updates

---
+++ precache-manifest.a82b41bc2bddeb89ce5cd321ef801ec9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c5923632ea28743847712c65613a48",
+    "url": "/robof/index.html"
+  },
+  {
+    "revision": "1f28232750824baee4a7",
+    "url": "/robof/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e55a7cb0ea8052b76b46",
+    "url": "/robof/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1f28232750824baee4a7",
+    "url": "/robof/static/js/2.250b9c49.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/robof/static/js/2.250b9c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e55a7cb0ea8052b76b46",
+    "url": "/robof/static/js/main.6d744176.chunk.js"
+  },
+  {
+    "revision": "7221b37f499a405293d7",
+    "url": "/robof/static/js/runtime-main.f55e852a.js"
+  }
+]);
