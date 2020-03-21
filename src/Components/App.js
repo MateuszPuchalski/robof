85461120e355ee568b0f6c53a44890c2b2407f0e
MateuszPuchalski <puchalskimateusz1@gmail.com>
2020-03-21 02:54:08 +0100

    changes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,42 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { connect } from "react-redux";
-import faker from "faker";
+import { useDispatch, useSelector } from "react-redux";
+
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import Scroll from "./Scroll";
 
-import { setSearchField } from "../actions";
+import { setSearchField, requestRobots } from "../actions";
 
-const mapStateToProps = state => {
-  return {
-    searchField: state.searchField
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    onSearchChange: event => dispatch(setSearchField(event.target.value))
-  };
-};
-
-function App({ searchField, onSearchChange }) {
-  const [robots, setRobots] = useState([]);
+export default function App({ store }) {
+  const dispatch = useDispatch();
   const [filteredRobots, setFilteredRobots] = useState([]);
+  const robots = useSelector(state => state.requestRobots.robots);
+  const searchField = useSelector(state => state.searchRobots.searchField);
 
-  const fakeRobots = x => {
-    const robotCards = [];
-    for (let i = 0; i < x; i++) {
-      const name = faker.name.findName();
-      const username = faker.internet.userName();
-      const email = faker.internet.email();
-      robotCards.push({ id: i + 1, name, username, email });
-    }
-    return robotCards;
+  const onSearchChange = evt => {
+    dispatch(setSearchField(evt.target.value));
   };
 
   useEffect(() => {
-    setRobots(fakeRobots(10));
-  }, []);
+    dispatch(requestRobots());
+  }, [dispatch]);
 
   useEffect(() => {
     const arr = robots.filter(robot => {
@@ -55,5 +38,3 @@ function App({ searchField, onSearchChange }) {
     </div>
   );
 }
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
